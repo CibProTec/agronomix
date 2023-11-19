791b41cbcd80781dff5e26f84759494aa8b42f9f
Jesus Marcano <jesusmarcanu24@gmail.com>
2023-11-19 18:51:44 -0500

    adding all the requests in apiService

--- src/apiService/apiService.js
+++ src/apiService/apiService.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:3001';
 
+/*--------------------------- Categorias -------------------------*/
+
 export const obtenerCategorias = () => {
   return axios.get(`${API_BASE_URL}/categorias`);
 };
@@ -25,14 +27,86 @@ export const obtenerInventario = () => {
   return axios.get(`${API_BASE_URL}/Inventario`);
 };
 
-export const crearInventario = (nuevaCategoria) => {
-  return axios.post(`${API_BASE_URL}/Inventario`, nuevaCategoria);
+export const crearInventario = (nuevoInventario) => {
+  return axios.post(`${API_BASE_URL}/Inventario`, nuevoInventario);
 };
 
-export const actualizarInventario = (id, categoriaActualizada) => {
-  return axios.put(`${API_BASE_URL}/Inventario/${id}`, categoriaActualizada);
+export const actualizarInventario = (id, InventarioActualizado) => {
+  return axios.put(`${API_BASE_URL}/Inventario/${id}`, InventarioActualizado);
 };
 
 export const eliminarInventario = (id, state) => {
   return axios.put(`${API_BASE_URL}/Inventario/${id}`, state);
+};
+
+/*--------------------------- Lotes -------------------------*/
+
+export const obtenerLotes = () => {
+  return axios.get(`${API_BASE_URL}/Lotes`);
+};
+
+export const crearLotes = (nuevoLote) => {
+  return axios.post(`${API_BASE_URL}/Lotes`, nuevoLote);
+};
+
+export const actualizarLotes = (id, LoteActualizado) => {
+  return axios.put(`${API_BASE_URL}/Lotes/${id}`, LoteActualizado);
+};
+
+export const eliminarLotes = (id, state) => {
+  return axios.put(`${API_BASE_URL}/Lotes/${id}`, state);
+};
+
+/*--------------------------- Productos -------------------------*/
+
+export const obtenerProductos = () => {
+  return axios.get(`${API_BASE_URL}/Productos`);
+};
+
+export const crearProductos = (nuevoProducto) => {
+  return axios.post(`${API_BASE_URL}/Productos`, nuevoProducto);
+};
+
+export const actualizarProductos = (id, ProductoActualizado) => {
+  return axios.put(`${API_BASE_URL}/Productos/${id}`, ProductoActualizado);
+};
+
+export const eliminarProductos = (id, state) => {
+  return axios.put(`${API_BASE_URL}/Productos/${id}`, state);
+};
+
+/*--------------------------- Unidad de Medida -------------------------*/
+
+export const obtenerUnidadMedida = () => {
+  return axios.get(`${API_BASE_URL}/UnidadMedida`);
+};
+
+export const crearUnidadMedida = (nuevaUnidadMedida) => {
+  return axios.post(`${API_BASE_URL}/UnidadMedida`, nuevaUnidadMedida);
+};
+
+export const actualizarUnidadMedida = (id, UnidadMedidaActualizada) => {
+  return axios.put(`${API_BASE_URL}/UnidadMedida/${id}`, UnidadMedidaActualizada);
+};
+
+export const eliminarUnidadMedida = (id, state) => {
+  return axios.put(`${API_BASE_URL}/UnidadMedida/${id}`, state);
+};
+
+/*--------------------------- Usuarios -------------------------*/
+
+export const obtenerUsuarios = () => {
+  return axios.get(`${API_BASE_URL}/Usuarios`);
+};
+
+export const crearUsuarios = (nuevoUsuario) => {
+  return axios.post(`${API_BASE_URL}/Usuarios`, nuevoUsuario);
+};
+
+export const actualizarUsuarios = (id, UsuariosActualizado) => {
+  return axios.put(`${API_BASE_URL}/Usuarios/${id}`, UsuariosActualizado);
+};
+
+export const eliminarUsuarios = (id, state) => {
+  return axios.put(`${API_BASE_URL}/Usuarios/${id}`, state);
 };
