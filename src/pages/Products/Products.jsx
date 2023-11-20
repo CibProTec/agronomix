65e637c23825fe14420661b085955207a1f1d829
Jesus Marcano <jesusmarcanu24@gmail.com>
2023-11-19 21:06:46 -0500

    adding create in categories view

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,13 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Table, Button, Form, FormGroup, Input, Row, Col } from 'reactstrap';
+import { Container,
+    Table,
+    Button,
+    Form,
+    FormGroup,
+    Input,
+    Modal,
+    Col,
+    Row,
+    ModalHeader,
+    ModalBody,
+    ModalFooter } from 'reactstrap';
 import deleteIcon from "../../assets/icons/delete-icon.png"
 import editIcon from "../../assets/icons/edit-icon.png"
 
-import { obtenerProductos } from "../../apiService/apiService";
+import { crearProductos, obtenerProductos } from "../../apiService/apiService";
 
 export const Products = () => {
 
     const [producto, setProducto] = useState([]);
+    const [imagen, setImagen] = useState(0);
+    const [nombre, setNombre] = useState(0);
+    const [idProducto, setIdProducto] = useState(0);
+    const [idLoteProducto, setIdLoteProducto] = useState(0);
+    const [idCategoriaProducto, setIdCategoriaProducto] = useState(0);
+    const [modal, setModal] = useState(false);
 
     useEffect(() => {
       obtenerProductos()
@@ -19,12 +36,55 @@ export const Products = () => {
           });
       }, []);
     
+      const toggleModal = () => {
+        setModal(!modal);
+        // Calcular automáticamente el próximo ID cuando se abre el modal para agregar categoría
+        const proximoId =
+          producto.length > 0
+            ? producto[producto.length - 1].idProducto + 1
+            : 1;
+        setIdProducto(proximoId);
+        setImagen("");
+        setNombre("");
+        setIdLoteProducto(0);
+        setIdCategoriaProducto(0);
+      };
+
+      const handleCrearProducto = () => {
+        const nuevoInventario = {
+          idProducto : idProducto,
+          imagen: imagen,
+          nombre: nombre,
+          idLoteProducto: idLoteProducto,
+          idCategoriaProducto: idCategoriaProducto,
+        };
+    
+        // Lógica para agregar una nueva categoría
+        crearProductos(nuevoInventario)
+          .then((response) => {
+            console.log(response.data);
+            // Actualizar la lista de categorías después de agregar una nueva categoría
+            obtenerProductos()
+              .then((response) => {
+                setProducto(response.data);
+              })
+              .catch((error) => {
+                console.error("Error fetching categories:", error);
+              });
+          })
+          .catch((error) => {
+            console.error("Error creating category:", error);
+          });
+    
+        // Cierra el modal después de agregar la categoría
+        toggleModal();
+      };
 
   return (
     <Container>
         <h4 className='mt-3 ms-1'>Productos</h4>
         <div className='d-flex justify-content-end'>
-            <Button className='bg-verde-bosque px-5 float-right me-2 mb-5'>Crear Producto</Button>
+            <Button className='bg-verde-bosque px-5 float-right me-2 mb-5' onClick={handleCrearProducto}>Crear Producto</Button>
         </div>
         <Form>
             <Row>
@@ -101,6 +161,67 @@ export const Products = () => {
                 )}
         </tbody>
         </Table>
+
+         {/* Modal para agregar Producto */}
+      <Modal isOpen={modal} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>Agregar Inventario</ModalHeader>
+        <ModalBody>
+          {/* Formulario para agregar Producto */}
+          <FormGroup>
+            <label htmlFor="idProducto">ID:</label>
+            <Input
+              type="text"
+              name="idProducto"
+              value={idProducto}
+              readOnly
+            />
+          </FormGroup>
+          <FormGroup>
+            <label htmlFor="imagen">imagen de producto:</label>
+            <Input
+              type="text"
+              name="imagen"
+              value={imagen}
+              onChange={(e) => setImagen(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <label htmlFor="Nombre">Nombre:</label>
+            <Input
+              type="text"
+              name="Nombre"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <label htmlFor="loteProducto">Lote del producto:</label>
+            <Input
+              type="text"
+              name="loteProducto"
+              value={idLoteProducto}
+              onChange={(e) => setIdLoteProducto(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup>
+            <label htmlFor="categoriaProducto">Categoria del producto:</label>
+            <Input
+              type="text"
+              name="categoriaProducto"
+              value={idCategoriaProducto}
+              onChange={(e) => setIdCategoriaProducto(e.target.value)}
+            />
+          </FormGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={handleCrearProducto}>
+            Agregar Inventario
+          </Button>
+          <Button color="secondary" onClick={toggleModal}>
+            Cancelar
+          </Button>
+        </ModalFooter>
+      </Modal>
     </Container>
 
   )
